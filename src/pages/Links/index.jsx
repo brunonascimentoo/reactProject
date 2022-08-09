4e7d2ee844ec699d84572679e80cf0b5fed95e95
Bruno Nascimento <89817533+capbruno@users.noreply.github.com>
2022-08-09 19:30:15 -0300

    Deletando botao youtube para acrescentar linkedin

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -9,10 +9,10 @@ export function Links() {
        <h3>Bruno Nascimento</h3>
        <span>@brunonnascimento_</span>
        </header>
-        <Button title='🎬 Youtube' links='https://www.youtube.com'/>
-        <Button title='📷 Instagram' links='https://www.instagram.com/brunonnascimento_/' />
-        <Button title='💻 Facebook' links='https://www.facebook.com/profile.php?id=100067153975680'/>
+        <Button title='💻 Linkedin' links='https://www.linkedin.com/in/brunonascimento1996/'/>
         <Button title='🐱 Github' links='https://github.com/capbruno'/>   
+        <Button title='📷 Instagram' links='https://www.instagram.com/brunonnascimento_/' />
+        <Button title='📱 Facebook' links='https://www.facebook.com/profile.php?id=100067153975680'/>
 
         <h4>Feito por Bruno Nascimento</h4>
         </Container>
